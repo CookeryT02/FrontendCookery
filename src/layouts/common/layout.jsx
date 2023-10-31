179485e81aba6a32c2794f7895a51cef93827f61
Zekai Kırıktaş <zkkiriktass@gmail.com>
2023-10-31 14:40:14 +0300

    route

--- src/layouts/common/layout.jsx
+++ src/layouts/common/layout.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Footer from "../../components/common/footer/footer"
+import Header from "../../components/common/header/header"
 
 const CommonLayout = () => {
   return (
     <div>
-      
+        <Header/>
+        <Outlet/>
+        <Footer/>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 // ADMIN PAGES
-import AdminContactMessagesPage from "./admin/contact-messages/page"
-import AdminContactMessageDetailsPage from "./admin/contact-messages/details/page"
-import AdminDashboard from "./admin/dashboard/page"
 
 // COMMON PAGES
-import AboutPage from "./common/about/page";
+import AboutPage from "./common/about/page"
 import ContactPage from "./common/contact/page";
-import HomePage from "./common/contact/page";
+import HomePage from "./common/home-page/page";
 
 export {
-    // ADMIN PAGES
-    AdminContactMessagesPage,
-    AdminContactMessageDetailsPage,
-    AdminDashboard,
+
 
     // COMMON PAGES
     AboutPage,
