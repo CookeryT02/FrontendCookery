f44fb16d069a162339957bb085d4108e086fe9ff
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-11-04 11:10:30 +0300

    backUp

--- src/components/common/header/top-bar/top-bar.jsx
+++ src/components/common/header/top-bar/top-bar.jsx
@@ -1,7 +1,58 @@
+import { Button, Col, Container, InputGroup, Navbar, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import "./top-bar.scss"
+import { Component, useState } from "react";
+
 const TopBar = () => {
+
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  const handleSearch = () => {
+    // earchText 
+  }
+
   return (
-    <div>TopBar</div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container className="tbContainer">
+
+          <Link to="/" title="Homepage">
+            <img src="/cookery-tech-logo.png" alt="" width={150} />
+          </Link>
+
+          <InputGroup className="inputGroup">
+            <input type="search"
+              name=""
+              id=""
+              value={searchText}
+              onChange={handleInputChange} />
+            <button className="searchButton" disabled={!searchText} onClick={handleSearch} >
+              <BiSearchAlt2
+                className="searchIcon"
+                size={20}
+
+              />
+            </button>
+          </InputGroup>
+
+          <span className="topBarIcons">
+            <AiOutlineHeart size={30} />
+            <HiOutlineShoppingBag size={30} />
+          </span>
+
+        </Container>
+      </Navbar>
+    </>
   )
+
+
 };
 
 export default TopBar;
